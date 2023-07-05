63f827f9d4e644764b017a4bdef691d0a72912a3
Socheat Sok <socheatsok78@gmail.com>
2023-07-05 13:03:49 +0700

    Update action outputs

--- action.sh
+++ action.sh
@@ -4,6 +4,8 @@ ALPINE_RELEASE_URL=${1}
 ALPINE_RELEASE_COUNT=${2}
 
 ALPINE_RELEASE_BRANCHES=$(curl -s "${ALPINE_RELEASE_URL}" | jq -c -r ".release_branches[:${ALPINE_RELEASE_COUNT}]")
+
+MATRIX_OS="[\"alpine\"]"
 MATRIX_VERSION=$(echo "$ALPINE_RELEASE_BRANCHES" | jq -r '.[].rel_branch | sub("v";"")' | jq -c --raw-input --slurp 'sub("v";"") | split("\n") | map(select(. != ""))')
 
 gh_set_output() {
@@ -11,6 +13,6 @@ gh_set_output() {
     echo "$1=$2" >> "$GITHUB_OUTPUT"
 }
 
-gh_set_output "os" "[\"alpine\"]"
+gh_set_output "os" "${MATRIX_OS}"
 gh_set_output "version" "${MATRIX_VERSION}"
-gh_set_output "matrix" "{\"os\": \"alpine\",\"version\": ${MATRIX_VERSION}}"
+gh_set_output "matrix" "{\"os\": ${MATRIX_OS}, \"version\": ${MATRIX_VERSION}}"
