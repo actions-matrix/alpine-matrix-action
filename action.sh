1ed90c096d03edf5a72abf4b3b3cbf17fcb5d73d
Socheat Sok <socheatsok78@gmail.com>
2023-07-05 12:39:41 +0700

    Init commit

---
+++ action.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+ALPINE_RELEASE_URL=${1}
+ALPINE_RELEASE_COUNT=${2}
+
+ALPINE_RELEASE_BRANCHES=$(curl -s "${ALPINE_RELEASE_URL}" | jq -c -r ".release_branches[:${ALPINE_RELEASE_COUNT}]")
+MATRIX_VERSION=$(echo "$ALPINE_RELEASE_BRANCHES" | jq -r '.[].rel_branch | sub("v";"")' | jq -c --raw-input --slurp 'sub("v";"") | split("\n") | map(select(. != ""))')
+
+echo "version=${MATRIX_VERSION}" >> "$GITHUB_OUTPUT"
