2c676bda3c0a499bb83ef4aa4febe7f8630a7585
Socheat Sok <socheatsok78@gmail.com>
2023-07-05 12:42:46 +0700

    Add os=alpine to github output

--- action.sh
+++ action.sh
@@ -6,4 +6,5 @@ ALPINE_RELEASE_COUNT=${2}
 ALPINE_RELEASE_BRANCHES=$(curl -s "${ALPINE_RELEASE_URL}" | jq -c -r ".release_branches[:${ALPINE_RELEASE_COUNT}]")
 MATRIX_VERSION=$(echo "$ALPINE_RELEASE_BRANCHES" | jq -r '.[].rel_branch | sub("v";"")' | jq -c --raw-input --slurp 'sub("v";"") | split("\n") | map(select(. != ""))')
 
+echo "os=alpine" >> "$GITHUB_OUTPUT"
 echo "version=${MATRIX_VERSION}" >> "$GITHUB_OUTPUT"
