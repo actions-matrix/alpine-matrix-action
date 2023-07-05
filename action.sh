5fed31a33b4e6a6f626143f6da78140d104962ad
Socheat Sok <socheatsok78@gmail.com>
2023-07-05 12:56:30 +0700

    Add logs

--- action.sh
+++ action.sh
@@ -6,6 +6,11 @@ ALPINE_RELEASE_COUNT=${2}
 ALPINE_RELEASE_BRANCHES=$(curl -s "${ALPINE_RELEASE_URL}" | jq -c -r ".release_branches[:${ALPINE_RELEASE_COUNT}]")
 MATRIX_VERSION=$(echo "$ALPINE_RELEASE_BRANCHES" | jq -r '.[].rel_branch | sub("v";"")' | jq -c --raw-input --slurp 'sub("v";"") | split("\n") | map(select(. != ""))')
 
-echo "os=alpine" >> "$GITHUB_OUTPUT"
-echo "version=${MATRIX_VERSION}" >> "$GITHUB_OUTPUT"
-echo "matrix={\"os\"=\"alpine\",\"version\"=\"${MATRIX_VERSION}\"}" >> "$GITHUB_OUTPUT"
+gh_set_output() {
+    echo "Set output: $1=$2"
+    echo "$1=$2" >> "$GITHUB_OUTPUT"
+}
+
+gh_set_output "os" "alpine"
+gh_set_output "version" "${MATRIX_VERSION}"
+gh_set_output "matrix" "{\"os\"=\"alpine\",\"version\"=\"${MATRIX_VERSION}\"}"
